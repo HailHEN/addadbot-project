dff94a9700f34df4eb39f4a3a21fd909faf9b4c0
Henok Hailu <hen.hail33@gmail.com>
2024-11-16 20:18:18 +1100

    no change

--- robotjobsite/job_analysis_functions/data_analysis.py
+++ robotjobsite/job_analysis_functions/data_analysis.py
@@ -2,33 +2,51 @@
 from django.db import models
 
 from .jobs import JobPosts
+import sklearn as sk
+import pandas as pd
 
 
 
 
+
+# convert all objects into pandas dataframe
 
 class JobPostingsAnalysis:
-    
-    def analyse_parameters(list_of_jobs):
+
+    def convert_to_dataframe(list_of_jobs):
+
         
 
 
 
-    def analyse_type_of_robotics(list_of_jobs):
 
-
-    def analyse_expertise_of_robotics_field(list_of_jobs):
-
-
-    def analyse_skills_required(list_of_jobs):
+    def __init__(self, list_of_jobs):
+        
+        self.dataframe_jobs_list = JobPostingsAnalysis.convert_to_dataframe(list_of_jobs=list_of_jobs)
 
 
 
-    def analyse_postings_date_created(list_of_jobs):
+    
+    def analyse_parameters():
+        
 
 
 
-    def analyse_job_position_posted(list_of_jobs):
+    def analyse_type_of_robotics():
+
+
+    def analyse_expertise_of_robotics_field():
+
+
+    def analyse_skills_required():
+
+
+
+    def analyse_postings_date_created():
+
+
+
+    def analyse_job_position_posted():
 
         
     
